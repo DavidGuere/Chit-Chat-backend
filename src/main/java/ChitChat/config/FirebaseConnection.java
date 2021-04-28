8f00b101695af71105644d1b9e020914c9ca1f84
GDavid <davidgallardoguere@gmail.com>
2021-04-28 03:11:01 +0200

    key

--- src/main/java/ChitChat/config/FirebaseConnection.java
+++ src/main/java/ChitChat/config/FirebaseConnection.java
@@ -3,13 +3,16 @@ package ChitChat.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class FirebaseConnection {
     public static void initializeFirebaseConnection() throws IOException {
+//        Dotenv dotenv = Dotenv.load();
         FileInputStream key = new FileInputStream("src/main/java/ChitChat/config/firestoreKey.json");
+//        FileInputStream key = new FileInputStream(dotenv.get("GG_CRED"));
         GoogleCredentials credentials = GoogleCredentials.fromStream(key);
         FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(credentials).build();
         FirebaseApp.initializeApp(options);
