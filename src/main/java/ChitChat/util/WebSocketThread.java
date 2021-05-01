ad5675f1108ae5756c23599320b3851cce9fa97d
GDavid <davidgallardoguere@gmail.com>
2021-05-01 03:41:17 +0200

    v1

--- src/main/java/ChitChat/util/WebSocketThread.java
+++ src/main/java/ChitChat/util/WebSocketThread.java
@@ -9,7 +9,8 @@ public class WebSocketThread implements Runnable{
     public void run() {
 //        String host = "localhost";
         String host = "chit-chatdegg.herokuapp.com/";
-        int port = process.env.PORT || 9000;
+//        int port = process.env.PORT || 9000;
+        int port = 9000;
         WebSocketConnection server = new WebSocketConnection(new InetSocketAddress(host, port));
         server.run();
     }
